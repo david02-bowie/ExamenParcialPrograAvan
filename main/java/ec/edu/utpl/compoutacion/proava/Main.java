a9171c0a691cc093e38919c5a6f3caecda4ab8d8
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2026-05-14 09:07:24 -0500

    Examen con ia

--- main/java/ec/edu/utpl/compoutacion/proava/Main.java
+++ main/java/ec/edu/utpl/compoutacion/proava/Main.java
@@ -1,15 +1,37 @@
 package ec.edu.utpl.compoutacion.proava;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+
+    public static void main(String[] args) {
         try {
-            String[][] matriz = {
+            String[][] matrizDatos = {
                     {"Java", "Python", "Java"},
                     {"C++", "Java", "Go"},
                     {"Java", "Rust", "Java"}
             };
+
+            String palabraBuscada = "Java";
+
+            matriz[] tareas = new matriz[matrizDatos.length];
+            Thread[] hilos = new Thread[matrizDatos.length];
+
+            for (int i = 0; i < matrizDatos.length; i++) {
+                tareas[i] = new matriz(matrizDatos[i], palabraBuscada, i + 1);
+                hilos[i] = new Thread(tareas[i]);
+                hilos[i].start();
+            }
+
+            for (int i = 0; i < hilos.length; i++) {
+                hilos[i].join();
+            }
+
+            int total = 0;
+
+            for (int i = 0; i < tareas.length; i++) {
+                total = total + tareas[i].getConteo();
+            }
+
+            System.out.println("Total de apariciones de la palabra " + palabraBuscada + ": " + total);
 
         } catch (Exception e) {
             throw new RuntimeException(e);
--- main/java/ec/edu/utpl/compoutacion/proava/matriz.java
+++ main/java/ec/edu/utpl/compoutacion/proava/matriz.java
@@ -1,47 +1,59 @@
 package ec.edu.utpl.compoutacion.proava;
 
 public class matriz implements Runnable {
+
     private String[] fila;
     private String palabraBuscada;
     private int conteo;
     private int numeroFila;
 
-    public matriz(String[] fila, String palabraBuscada, int numerofila) {
+    public matriz(String[] fila, String palabraBuscada, int numeroFila) {
         setFila(fila);
         setPalabraBuscada(palabraBuscada);
         setNumeroFila(numeroFila);
         setConteo(0);
     }
 
+    @Override
+    public void run() {
+        for (int i = 0; i < getFila().length; i++) {
+            if (getFila()[i].equalsIgnoreCase(getPalabraBuscada())) {
+                setConteo(getConteo() + 1);
+            }
+        }
+
+        System.out.println("Fila " + getNumeroFila() + ": " + getConteo() + " apariciones");
+    }
+
     public String[] getFila() {
         return fila;
     }
 
-    public void setFila(String[] fila) {
-        this.fila = fila;
+    public void setFila(String[] filaNueva) {
+        fila = filaNueva;
     }
 
     public String getPalabraBuscada() {
         return palabraBuscada;
     }
 
-    public void setPalabraBuscada(String palabraBuscada) {
-        this.palabraBuscada = palabraBuscada;
+    public void setPalabraBuscada(String palabraNueva) {
+        palabraBuscada = palabraNueva;
     }
 
     public int getConteo() {
         return conteo;
     }
 
-    public void setConteo(int conteo) {
-        this.conteo = conteo;
+    public void setConteo(int conteoNuevo) {
+        conteo = conteoNuevo;
     }
 
     public int getNumeroFila() {
         return numeroFila;
     }
 
-    public void setNumeroFila(int numeroFila) {
-        this.numeroFila = numeroFila;
+    public void setNumeroFila(int numeroFilaNueva) {
+        numeroFila = numeroFilaNueva;
     }
 }
